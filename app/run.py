30c4b3c9436c9a1f98987591df30329e7e7b82b6
Azeez Liadi <liadiazeez3@gmail.com>
2023-12-12 21:30:12 +0100

    update files

--- app/run.py
+++ app/run.py
@@ -114,3 +114,11 @@ def go():
         query=query,
         classification_result=classification_results
     )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3000, debug=True)
+
+
+if __name__ == '__main__':
+    main()
